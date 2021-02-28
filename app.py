d0dacb861b90a2fb2ecf080b01459a215478d254
khabiirk <khabiir.khodabaccus@mail.mcgill.ca>
2021-02-28 23:20:43 +0400

    initial commit for heroku

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify, send_file
+from srcnn import SRCNN
+from PIL import Image
+from flask_cors import CORS, cross_origin
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
+from os import remove
+import base64
+import re
+import io
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+srcnn = SRCNN()
+
+@app.route('/', methods=['GET', 'POST'])
+def load():
+    return render_template('base.html')
+
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+    
+    f = request.files['image']
+    
+    print(f)
+    prediction, score = srcnn.predict(f)
+    # img = Image.fromarray(prediction.astype('uint8'))
+    # file_obj = io.BytesIO()
+
+    # img.save(file_obj, 'PNG')
+    # file_obj.seek(0)
+
+    # tempFileObj = NamedTemporaryFile(mode='w+b', suffix='png')
+    # with open('/Users/khabiirkhodabaccus/Desktop/tutorial/SRCNN-webapp/static/tmp.png', 'rb') as tmp:
+    #      encoded_string = base64.b64encode(tmp.read())
+         #copyfileobj(tmp, tempFileObj)
+
+    encoded_string = base64.b64encode(prediction)
+    
+    # remove("/Users/khabiirkhodabaccus/Desktop/tutorial/SRCNN-webapp/static/tmp.png")
+    # tempFileObj.seek(0,0)
+
+    # response = send_file(tempFileObj, as_attachment=True, attachment_filename='predicted.png')
+    result = {'data' : encoded_string.decode('utf-8'), 'score' : score}
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
---
+++ srcnn.py
@@ -0,0 +1,92 @@
+import sys
+import keras
+import cv2
+import numpy as np
+import matplotlib
+import math
+from matplotlib import pyplot as plt
+import skimage
+import os
+from PIL import Image
+
+def psnr(target, ref):
+         
+    # assume RGB image
+    target_data = target.astype(float)
+    ref_data = ref.astype(float)
+
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+
+    rmse = math.sqrt(np.mean(diff ** 2.))
+
+    return 20 * math.log10(255. / rmse)
+def compare_images(target, ref):
+    
+    return psnr(target, ref)
+
+    
+    
+def modcrop(img, scale):
+    #print(img)
+    tmpsz = img.shape
+    sz = tmpsz[0:2]
+    sz = sz - np.mod(sz, scale)
+    img = img[0:sz[0], 1:sz[1]]
+    return img
+
+
+def shave(image, border):
+    img = image[border: -border, border: -border]
+    return img
+
+class SRCNN:
+    def __init__(self):
+        self.model = keras.models.load_model('model/mo.h5')
+        self.model.load_weights('model/3051crop_weight_200.h5')
+    
+
+    def predict(self, request):
+        
+
+        r = Image.open(request)
+        
+
+        
+        #print(file)
+        degraded = cv2.cvtColor(np.array(r), cv2.COLOR_RGB2BGR)
+        # preprocess the image with modcrop
+
+        degraded = modcrop(degraded, 3)
+        temp = cv2.cvtColor(degraded, cv2.COLOR_BGR2YCrCb)
+        Y = np.zeros((1, temp.shape[0], temp.shape[1], 1), dtype=float)
+        Y[0, :, :, 0] = temp[:, :, 0].astype(float) / 255
+        
+        # perform super-resolution with srcnn
+        pre = self.model.predict(Y, batch_size=1)
+        
+        # post-process output
+        pre *= 255
+        pre[pre[:] > 255] = 255
+        pre[pre[:] < 0] = 0
+        pre = pre.astype(np.uint8)
+        
+        # copy Y channel back to image and convert to BGR
+        temp = shave(temp, 6)
+        temp[:, :, 0] = pre[0, :, :, 0]
+        output = cv2.cvtColor(temp, cv2.COLOR_YCrCb2BGR)
+        degraded = shave(degraded.astype(np.uint8), 6)
+        score = compare_images(degraded, output)
+        retval, buffer_img = cv2.imencode('.png', output)
+        #cv2.imwrite('/Users/khabiirkhodabaccus/Desktop/tutorial/SRCNN-webapp/static/tmp.png', output)
+       
+        # return image
+        return buffer_img, score
+
+
+# o = SRCNN()
+# output = o.predict('8.png')
+# print(output)
+# #cv2.imwrite('test.png', output)
+# fig, axs = plt.subplots(1, 3, figsize=(20, 8))
+# axs[0].imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
